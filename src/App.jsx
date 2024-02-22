294a88e6196f358087372a6d7a9ba9c7c0460955
mibri-hub <mihalka.gabi@gmail.com>
2024-02-22 12:31:23 +0100

    first

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import React,{ useEffect, useState } from 'react'
+import './App.css'
+import { Todos } from './components/Todos'
+import { Login} from './components/Login'
+import { getCurrentUser, signOutUser } from './utils'
+import LogoutIcon from '@mui/icons-material/Logout';
+
+
+
+function App() {
+  const [user,setUser]=useState(null)
+  getCurrentUser(setUser)
+
+  //console.log(user);
+  return (
+    
+      <div className="app">
+     <h1>My Todo</h1>
+    
+     {user && <LogoutIcon
+     sx={{width:"100%",cursor:"pointer",color:"black"}}
+     onClick={()=>signOutUser()}
+     />}
+     {user==null ? <Login /> : <Todos/>}
+      </div>  
+  )
+}
+
+export default App
---
+++ src/components/Todo.jsx
@@ -0,0 +1,55 @@
+import * as React from 'react';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import Checkbox from '@mui/material/Checkbox';
+import IconButton from '@mui/material/IconButton';
+import DeleteIcon from '@mui/icons-material/Delete';
+import { deleteTodo, toggleDone } from '../utils';
+import CreateIcon from '@mui/icons-material/Create';
+import { EditTodo } from './EditTodo';
+
+export const Todo=({id,descr,done})=> {
+  const [open, setOpen] = React.useState(false);
+
+  //console.log(id);
+        return (
+         <> 
+          <ListItem
+            key={id}
+            secondaryAction={
+              <>
+              <IconButton edge="end" aria-label="comments"
+                onClick={()=>deleteTodo(id)}
+              >
+              <DeleteIcon sx={{color:'gray'}}/>
+              </IconButton>
+
+              <IconButton edge="end" aria-label="comments"
+                onClick={()=>setOpen(true)}
+            >
+              <CreateIcon sx={{color:'blue'}}/>
+            </IconButton>
+            </>
+            }
+            disablePadding
+          >
+            <ListItemButton role={undefined}  dense
+              onClick={()=>toggleDone(id,!done)}>
+              <ListItemIcon>
+                <Checkbox
+                  edge="start"
+                  checked={done}
+                  tabIndex={-1}
+                  disableRipple
+                  inputProps={{ 'aria-labelledby': id }}
+                />
+              </ListItemIcon>
+              <ListItemText id={id} primary={descr} />
+            </ListItemButton>
+          </ListItem>
+          {open && <EditTodo open={open} setOpen={setOpen} id={id} descr={descr}/>}
+          </>
+        );
+}
